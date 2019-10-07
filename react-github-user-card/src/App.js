fb5a22380602cdf43fca7fea072bdb1dec59c2f9
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-10-06 19:04:12 -0500

    set userInfo to state

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -28,7 +28,9 @@ export default class App extends Component {
  getUserData = () => {
    axios.get(`http://api.github.com/users/${this.state.userName}`)
     .then(res => {
-      console.log(res)
+      this.setState({
+        userInfo: res.data
+      })
     })
     .catch(err => console.log(err))
  }
@@ -61,7 +63,21 @@ export default class App extends Component {
         </form>
        
       <div>
-
+      {/* <div class="card">
+        <img src={this.state.userInfo.avatar_url} />
+        <div>
+          <h3>{}</h3>
+          <p class="username">{users user name}</p>
+          <p>Location: {users location}</p>
+          <p>Profile:  
+            <a href={address to users github page}>{address to users github page}</a>
+          </p>
+          <p>Followers: {users followers count}</p>
+          <p>Following: {users following count}</p>
+          <p>Bio: {users bio}</p>
+        </div>
+      </div> */}
+        <img src={this.state.userInfo.avatar_url} alt="github user" />
       </div>
        
       </>
